07282e7e7c45693996aa505eeb0b61b837934c86
DavidDontKnow <daviddontknow02@gmail.com>
2023-09-03 12:53:20 -0700

    first commit

---
+++ program.py
@@ -0,0 +1,38 @@
+import random
+
+
+def get_choices():
+    player_choice = input("Enter a choice: Rock Paper or Scissors, ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+
+    return choices
+
+
+def check_win(player, computer):
+    print(f"You chose: {player}, computer chose: {computer}")
+    if player == computer:
+        return ("It is a tie")
+    elif player == "rock":
+        if computer == "scissors":
+            return "Player Wins!"
+        else:
+            return "Computer Wins!"
+    elif player == "paper":
+        if computer == "rock":
+            return "Player Wins!"
+        else:
+            return "Computer Wins!"
+    elif player == "Scissors":
+        if computer == "paper":
+            return "Player Wins!"
+        else:
+            return "Computer Wins!"
+
+
+choices = get_choices()
+
+result = check_win(choices["player"], choices["computer"])
+
+print(result)
